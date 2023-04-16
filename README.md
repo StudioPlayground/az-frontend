697deb2d1c61dd5615d8113fe3cc36bd476b3602
96glory <click_syk@naver.com>
2023-04-16 18:05:11 +0900

    :sparkles: eslint 추가 및 적용

--- README.md
+++ README.md
@@ -18,3 +18,18 @@ docker build -t az-frontend .
 # run container
 docker run -d -p 3000:3000 --name az-frontend az-frontend
 ```
+
+## lint 적용
+
+- vscode extension 설치
+  - ESlint
+  - prettier
+- vscode settings 아래 옵션 설정
+  - format on save: `true`
+  - default formmater: `prettier`
+
+```shell
+yarn lint
+```
+
+- 참조 : https://dev.to/eshankvaish/set-up-eslint-and-prettier-in-a-react-typescript-app-2022-29c9
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
 import '@/main.css';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
 import { AzitRouter } from '@router/AzitRouter';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
       <BrowserRouter>
